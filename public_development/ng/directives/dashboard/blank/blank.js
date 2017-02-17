c3b1a0e8a2a95591e9c53d0d989ca5e0430877d9
rakesh <rak1994kumar@gmail.com>
2017-02-17 17:18:46 +0530

    sdsdasd

--- public_development/ng/directives/dashboard/blank/blank.js
+++ public_development/ng/directives/dashboard/blank/blank.js
@@ -1,12 +1,31 @@
 ' use strict';
 
 angular.module('crewMeanApp')
-    .directive('blank', ['$compile', function($compile) {
+    .directive('blank', ['$compile', function ($compile) {
         return {
             restrict: 'E',
             templateUrl: 'ng/directives/dashboard/blank/blank.tmpl.html',
-            controller: function($scope, $state) {
-                
+            controller: function ($scope, $state, $http) {
+
+                $scope.crewTypeList = [];
+                $scope.query = {
+                    limit:10,
+                    page:1,
+                    sortBy:'name'
+                };
+
+                $http.get("http://localhost:4000/api/v1/crewTypes",{params:$scope.query}).then(function successResponse(response) {
+
+                    $scope.crewTypeList = response.data.results;
+                    $scope.currentPage = response.data.current;
+                    $scope.perPage = response.data.options.perPage;
+                    $scope.totalPages = response.data.last;
+                    $scope.totalRecords = response.data.count;
+
+                }, function errorResponse(response) {
+
+
+                });
             }
 
         };
