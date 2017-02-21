93ed1b6b077fd497b7ed8b89ee268596db7ac83f
rakesh <rak1994kumar@gmail.com>
2017-02-21 15:47:45 +0530

    csaasdas

--- public_development/ng/directives/dashboard/sidebar/sidebar.js
+++ public_development/ng/directives/dashboard/sidebar/sidebar.js
@@ -1,53 +1,42 @@
 (function () {
-
-
     ' use strict';
-
-    angular.module('crewMeanApp')
-        .directive('sidebar', ['$compile', function ($compile) {
+var app = angular.module('crewMeanApp');
+        app.directive('sidebar', ['$compile', function ($compile) {
             return {
                 restrict: 'E',
                 templateUrl: 'ng/directives/dashboard/sidebar/sidebar.tmpl.html',
                 replace: true,
                 scope: {},
                 controller: function ($scope, $state, $window, $location, UserService) {
-
-
-
                     $scope.UserService = UserService;
                     $scope.hideSidebarVar = false;
-
 
                     $scope.$watch(function () {
                         return UserService.config.sidebarTrigger;
                     }, function (newVal) {
 
                         if (!newVal)
-                            $scope.disableSidebar();
+                            disableSidebar();
                         else
-                            $scope.enableSidebar();
+                            enableSidebar();
                         $scope.hideSidebarVar = !newVal;
                     }.bind(this), true);
 
 
-                    $scope.hideSidebar = function () {
-
+                    $scope.hideSidebar = function() {
                         $scope.hideSidebarVar = ($scope.hideSidebarVar) ? false : true;
                         UserService.config.sidebarTrigger = !$scope.hideSidebarVar;
                         console.log("sidebar");
 
                     };
 
-
-                    $scope.disableSidebar = function () {
-
+                    var disableSidebar = function () {
                         console.log("disable Sidebar");
-
                     };
 
-                    $scope.enableSidebar = function () {
+                    var enableSidebar = function () {
                         console.log("enable Sidebar");
-                    }
+                    };
 
 
                 }
