f50fdc0f4a48461a68c23c4c9081295615884fa3
rakesh <rak1994kumar@gmail.com>
2017-02-21 15:22:08 +0530

    sdasdasd

--- public_development/ng/directives/dashboard/blank/blank.js
+++ public_development/ng/directives/dashboard/blank/blank.js
@@ -1,37 +1,19 @@
-' use strict';
+(function () {
+    ' use strict';
 
-angular.module('crewMeanApp')
-    .directive('blank', ['$compile', function ($compile) {
-        return {
-            restrict: 'E',
-            templateUrl: 'ng/directives/dashboard/blank/blank.tmpl.html',
-            controller: function ($scope, $state, $http,UserService) {
-
-  //console.log(""+UserService.getUserPlan());
-  //console.log(""+UserService.getCurrentUser());
-  //console.log(""+UserService.getUser());
-
-  
-                $scope.crewTypeList = [];
-                $scope.query = {
-                    limit:10,
-                    page:1,
-                    sortBy:'name'
-                };
-
-                $http.get(crewTypeUri,{params:$scope.query}).then(function successResponse(response) {
-
-                    $scope.crewTypeList     = response.data.results;
-                    $scope.currentPage      = response.data.current;
-                    $scope.perPage          = response.data.options.perPage;
-                    $scope.totalPages       = response.data.last;
-                    $scope.totalRecords     = response.data.count;
-
-                }, function errorResponse(response) {
+    angular.module('crewMeanApp')
+        .directive('blank', ['$compile', function ($compile) {
+            return {
+                restrict: 'E',
+                templateUrl: 'ng/directives/dashboard/blank/blank.tmpl.html',
+                controller: function ($scope, $state, $http, UserService) {
 
 
-                });
-            }
 
-        };
-    }]);
+                }
+
+            };
+        }]);
+
+})();
+
--- public_development/ng/directives/dashboard/sidebar/sidebar.js
+++ public_development/ng/directives/dashboard/sidebar/sidebar.js
@@ -1,51 +1,58 @@
-' use strict';
-
-angular.module('crewMeanApp')
-    .directive('sidebar', ['$compile', function ($compile) {
-        return {
-            restrict: 'E',
-            templateUrl: 'ng/directives/dashboard/sidebar/sidebar.tmpl.html',
-            controller: function ($scope, $state, $window, $location, UserService) {
+(function () {
 
 
+    ' use strict';
 
-                $scope.UserService = UserService;
-                $scope.hideSidebarVar = false;
-
-
-                $scope.$watch(function () { 
-                    return UserService.config.sidebarTrigger; 
-                }, function (newVal) {
-
-                    if (!newVal)
-                        $scope.disableSidebar();
-                    else
-                        $scope.enableSidebar();
-                    $scope.hideSidebarVar = !newVal;
-                }.bind(this), true);
+    angular.module('crewMeanApp')
+        .directive('sidebar', ['$compile', function ($compile) {
+            return {
+                restrict: 'E',
+                templateUrl: 'ng/directives/dashboard/sidebar/sidebar.tmpl.html',
+                replace: true,
+                scope: {},
+                controller: function ($scope, $state, $window, $location, UserService) {
 
 
-                $scope.hideSidebar = function () {
 
-                    $scope.hideSidebarVar = ($scope.hideSidebarVar) ? false : true;
-                    UserService.config.sidebarTrigger = !$scope.hideSidebarVar;
-                    console.log("sidebar");
-
-                };
+                    $scope.UserService = UserService;
+                    $scope.hideSidebarVar = false;
 
 
-                $scope.disableSidebar = function () {
+                    $scope.$watch(function () {
+                        return UserService.config.sidebarTrigger;
+                    }, function (newVal) {
 
-                    console.log("disable Sidebar");
+                        if (!newVal)
+                            $scope.disableSidebar();
+                        else
+                            $scope.enableSidebar();
+                        $scope.hideSidebarVar = !newVal;
+                    }.bind(this), true);
 
-                };
 
-                $scope.enableSidebar = function () {
-                    console.log("enable Sidebar");
+                    $scope.hideSidebar = function () {
+
+                        $scope.hideSidebarVar = ($scope.hideSidebarVar) ? false : true;
+                        UserService.config.sidebarTrigger = !$scope.hideSidebarVar;
+                        console.log("sidebar");
+
+                    };
+
+
+                    $scope.disableSidebar = function () {
+
+                        console.log("disable Sidebar");
+
+                    };
+
+                    $scope.enableSidebar = function () {
+                        console.log("enable Sidebar");
+                    }
+
+
                 }
 
+            };
+        }]);
 
-            }
-
-        };
-    }]);
+})();
